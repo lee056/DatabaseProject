dc29ce8e9a3038973fdbca03084f56f535845de0
AnnaliseHarms <akharms@uwm.edu>
2025-04-08 19:42:01 -0500

    updated_features

--- movies_matrix/views.py
+++ movies_matrix/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+
+def login_redirect(request):
+    return redirect('login')  #to login page
+
+def create_account(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid(): #if details are valid 
+            form.save() #save user
+            return redirect('login') #sends to login after create account
+    else:
+        form = CustomUserCreationForm() 
+    return render(request, 'create_account.html', {'form': form})
+
+
+def home(request):
+    return render(request, 'homepage.html') #to homepage
+
