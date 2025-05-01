01ca0302e4de303ebade3635c6852f7120305e84
pongoeoe <hudkolowrat@yahoo.com>
2025-05-01 17:02:47 -0500

    search working

--- movies_matrix/views.py
+++ movies_matrix/views.py
@@ -1,38 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
-from django.db.models import Search
 from .models import Movie
-
+from django.db.models import Q
 
 def login_redirect(request):
-    return redirect('login')  #to login page
+    return redirect('login')  # to login page
 
 def create_account(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
-        if form.is_valid(): #if details are valid 
+        if form.is_valid(): #if details are valid
             form.save() #save user
             return redirect('login') #sends to login after create account
     else:
-        form = CustomUserCreationForm() 
+        form = CustomUserCreationForm()
     return render(request, 'create_account.html', {'form': form})
 
 
 def home(request):
     return render(request, 'homepage.html') #to homepage
 
-    from django.shortcuts import render
-    
 
 def search_results(request):
     query = request.GET.get('search')
     if query:
-            # Using icontains for case-insensitive search
         results = Movie.objects.filter(
-            Search(title__icontains=query) | Search(author__icontains=query) | Search(description__icontains=query)
-        )
+            Q(movie_title__icontains=query) |
+            Q(casting__actor_actor__actor_first_name__icontains=query) |
+            Q(casting__actor_actor__actor_last_name__icontains=query) |
+            Q(genremovieassignment__genre_genre__genre_name__icontains=query) |
+            Q(moviedirectorassignment__director_director__director_first_name__icontains=query) |
+            Q(moviedirectorassignment__director_director__director_last_name__icontains=query)
+        ).distinct()
     else:
         results = Movie.objects.all()  # Or handle empty query as needed
 
-    return render(request, 'search_results.html', {'results': results, 'query': query})
+    return render(request, 'results.html', {'results': results, 'query': query})
